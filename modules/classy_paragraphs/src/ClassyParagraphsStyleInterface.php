aafa3fbc177794548b527bf9e571383e23944516
Nicolas Borda <nborda@isovera.com>
2016-06-30 17:41:44 -0500

    Checking in contrib modules

---
+++ modules/classy_paragraphs/src/ClassyParagraphsStyleInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\classy_paragraphs\ClassyParagraphsStyleInterface.
+ */
+
+namespace Drupal\classy_paragraphs;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+ * Provides an interface for defining Classy paragraphs style entities.
+ */
+interface ClassyParagraphsStyleInterface extends ConfigEntityInterface {
+
+  /**
+   * Returns text from the classes field.
+   *
+   * @return
+   *   A string of classes.
+   */
+  public function getClasses();
+
+}
